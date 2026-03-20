4f4a26004f348586fb252eb57e886df0668c47ee
unknown <santhoshkumar709488@gmail.com>
2026-03-20 22:12:24 +0530

    folder structure added

---
+++ templetate.sh
@@ -0,0 +1,18 @@
+# folder creation
+
+mkdir -p research
+mkdir -p src
+
+
+# creating files
+
+touch src/prompt.py
+touch src/helper.py
+touch src/__init__.py
+touch .env
+touch app.py
+
+touch research/trials.ipynb
+touch requirements.txt
+
+echo " files created successfully"
